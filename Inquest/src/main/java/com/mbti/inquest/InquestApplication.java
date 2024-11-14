67ab0a84c106a7de8e7729e16aa85443e6a13d6e
dpfls-space <38265ye@gmail.com>
2024-11-14 16:15:19 +0900

    Toy Project

---
+++ Inquest/src/main/java/com/mbti/inquest/InquestApplication.java
@@ -0,0 +1,13 @@
+package com.mbti.inquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InquestApplication.class, args);
+	}
+
+}
---
+++ Inquest/src/test/java/com/mbti/inquest/InquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbti.inquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
